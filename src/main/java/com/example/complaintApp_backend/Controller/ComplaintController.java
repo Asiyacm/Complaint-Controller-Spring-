c817d8cacd3a853619ca4a26c67b685d8ed4e20c
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-11 22:16:55 +0530

    userregister commit

--- src/main/java/com/example/complaintApp_backend/Controller/ComplaintController.java
+++ src/main/java/com/example/complaintApp_backend/Controller/ComplaintController.java
@@ -1,7 +1,34 @@
 package com.example.complaintApp_backend.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.complaintApp_backend.Model.Complaint;
+import com.example.complaintApp_backend.dao.ComplaintDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class ComplaintController {
+    @Autowired
+    private ComplaintDao dao;
+    @CrossOrigin("*")
+    @PostMapping("/")
+    public String AdminLogin(){
+        return "Admin";
+    }
+@CrossOrigin("*")
+    @PostMapping(path="/register",consumes = "application/json",produces="application/json")
+    public HashMap<String, String> UserRegister(@RequestBody Complaint c){
+        System.out.println(c.getName().toString());
+        dao.save(c);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
+}
+
+
+    @PostMapping("/signin")
+    public String UserLogin(){
+        return "Login";
+    }
 }
